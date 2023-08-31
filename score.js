88fd64779df1f3aa3ac974a07be7433a05862ea9
contactmeroshan <contactmeroshan@gmail.com>
2023-08-31 22:28:25 +0530

    k value KNN working for 10,1112. we can try 10

--- score.js
+++ score.js
@@ -1,33 +1,53 @@
-const outputs = []
-const predictionpoint = 300;
-const k = 3;
-
-
-function distancefrompredictionpoint(point)
-{
-  return Math.abs(point - predictionpoint)
-}
+const outputs = [];
+// const predictionpoint = 300;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
-  outputs.push([dropPosition, bounciness, size, bucketLabel])
-
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
- const bucket = _.chain(outputs)
-		.map( eacharray => [distancefrompredictionpoint(eacharray[0]), eacharray[3]] )
-		.sortBy(eacharray => eacharray[0])
-		.slice(0, k)
-		.countBy(eacharray => eacharray[1])
-		.toPairs()
-		.sortBy()
-		.last()
-		.first()
-		.parseInt()
-		.value()
-//  CONSOLE OUTPUT 
-    console.log("most probable bucket number is :", bucket);
+  const testsetsize = 100;
+  const [testSet, trainingSet] = SplitDataSet(outputs, testsetsize);
+  _.range(1, 20).forEach((k) => {
+    const accuracy = _.chain(testSet) // this will take ARRAYS from 'testset' shuffled arrays previously made.
+      .filter(
+        (testsetarray) =>
+          knn(trainingSet, testsetarray[0], k) === testsetarray[3],
+      ) // comparing THE BUCKET coming out of KNN algorithm on "trainingset" to match the same bucket coming from the data of "testset"
+      .size() // its like length of array from usng the LODASH LIBRARY , method chaining is easy!!
+      .divide(testsetsize) // test whether same bucket prediction matches with the TESTSETSIZE , thus giving percentage of success below.
+      .value();
+    const accuracyPercentage = accuracy / testsetsize;
+    console.log("for k of ", k, "your accuracy is : ", accuracyPercentage);
+  });
+}
+//KNN algorithm implementation in a very crisp manner using LODASH library giving TOP K results giving max non-zero predictions
+function knn(data, point, k) {
+  return _.chain(data)
+    .map((eacharray) => [distance(eacharray[0], point), eacharray[3]])
+    .sortBy((eacharray) => eacharray[0])
+    .slice(0, k)
+    .countBy((slicedArray) => slicedArray[1])
+    .toPairs()
+    .sortBy()
+    .last()
+    .first()
+    .parseInt()
+    .value();
+  //  CONSOLE OUTPUT
+  // console.log("most probable bucket number is :", bucket);
 }
 
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function SplitDataSet(data, testcount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testcount); // split from O to TESTCOUNT of SHUFFLED Datasource
+  const trainingSet = _.slice(shuffled, testcount); // split from TESTCOUNT to the end(is default when not mentioned)
+  return [testSet, trainingSet];
+}
